0063b7e797871dd76059b4bd56f142e42279a01a
TamNguyen <huutam.ptnk@gmail.com>
2018-06-25 13:03:21 +0700

    modify index.js

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/api/courses', (req, res) => {
 	res.send([1, 2, 3]);
 });
 
+app.get('/api/post/:year/:month', (req, res) => {
+	// req.params is an object
+	res.send(req.params);
+	// go to this address to see this query (behind ?) 
+	// http://127.0.0.1:3000/api/post/2017/1?sortBy=name
+	res.send(req.query);
+});
 
 // if process.env.PORT is set, we'll use it, otherwise we'll use 3000
 const port = process.env.PORT || 3000;
