818639b1a1e014d1e270111e3a30429f2928aed3
TamNguyen <huutam.ptnk@gmail.com>
2018-06-27 00:11:50 +0700

    added put request

--- index.js
+++ index.js
@@ -33,16 +33,16 @@ app.get('/api/courses/:id', (req, res) => {
 // post request
 // never trust what the client sends!
 app.post('/api/courses', (req, res) => {
-	const schema = {
-		name: Joi.string().min(3).required()
-	};
-	const result = Joi.validate(req.body, schema);
-	console.log(result);
-	if (result.error){
+	// const result = validateCourse(req.body);
+	const {error} = validateCourse(req.body);
+	//if (result.error){
+	if (error){
 		res.status(400).send(result.error.details[0].message);
 		// return cause we dont what the later to be executed.
 		return;
 	}
+	console.log(result);
+	
 	const course = {
 		id: courses.length +1,
 		name: req.body.name
@@ -59,6 +59,38 @@ app.get('/api/post/:year/:month', (req, res) => {
 	// http://127.0.0.1:3000/api/post/2017/1?sortBy=name
 	res.send(req.query);
 });
+
+// Modify an object
+app.put('/api/courses/:id',(req, res) => {
+	// Look up the course
+	const course = courses.find(c => c.id === parseInt(req.params.id));
+	if (!course) 
+		// If not existing, return 404
+		res.status(404).send("Course's id not found!");
+	
+	// Validate
+	// If invalid, return 400 - bad request
+	// const result = validateCourse(req.body);
+	const {error} = validateCourse(req.body);
+	//if (result.error){
+	if (error){
+		res.status(400).send(result.error.details[0].message);
+		// return cause we dont what the later to be executed.
+		return;
+	}
+	
+	// If valid, update te course
+	course.name = req.body.name;
+	// Return update course
+	res.send(course);
+});
+
+function validateCourse(course){
+	const schema = {
+		name: Joi.string().min(3).required()
+	};
+	return Joi.validate(course, schema);
+}
 
 // if process.env.PORT is set, we'll use it, otherwise we'll use 3000
 const port = process.env.PORT || 3000;
