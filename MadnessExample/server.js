4fb08871211c9f7a653f7472d3519c886aca5722
Aleadinglight <huutamnguyen2302@gmail.com>
2019-10-08 01:55:40 +0300

    update server.js

--- MadnessExample/server.js
+++ MadnessExample/server.js
@@ -3,28 +3,12 @@ const app = express();
 const config = require("./src/config/config");
 const path = require("path");
 
+const api = require('./routes/api');
+
+
 app.use(express.json());
 
-app.get("/api/location", (req, res) => {
-    res.status(200).send("All location");
-});
-
-app.get("/api/location/:id", (req, res) => {
-    var id = parseInt(req.params.id);
-    res.status(200).send("Location with id: "+ id);
-});
-
-app.post("/api/location", (req, res) => {
-    res.status(200).send("Create location");
-});
-
-app.put("/api/location/:id", (req, res) => {
-    res.status(200).send("Edit location");
-});
-
-app.delete("/api/location/:id", (req, res) => {
-    res.status(200).send("Delete location");
-});
+app.use('/api', api); 
 
 app.get("*", (req, res) => {
     res.status(404).send("404 not found");
